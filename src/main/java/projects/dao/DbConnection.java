d88d928022f5895af1644a86b7daaedc86ae6f88
Shawn Bataju <bshawngreen@gmail.com>
2022-10-22 17:50:39 -0500

    Added coding assignment pd with links to repo and video

--- src/main/java/projects/dao/DbConnection.java
+++ src/main/java/projects/dao/DbConnection.java
@@ -7,15 +7,22 @@ import java.sql.SQLException;
 import projects.exception.DbException;
 
 public class DbConnection {
+	//Constants
 	private static String HOST = "localhost";
 	private static String PASSWORD = "projects";
 	private static int PORT = 3306;
 	private static String SCHEMA = "projects";
 	private static String USER = "projects";
 	
+	/*
+	 * This method returns a Connection object if the connection is successful,
+	 * and prints an error message if the connection is not successful.
+	 */
 	public static Connection getConnection() {
+		//Creating a MySQL connection URL using the constants
 		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s"
 				, HOST, PORT, SCHEMA, USER, PASSWORD);
+		//Checked exception returns Connection if successful:
 		Connection conn;
 		try {
 			conn = DriverManager.getConnection(uri);
